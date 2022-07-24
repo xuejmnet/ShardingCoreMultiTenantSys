5ed012f91ec98ab1ee80f7096c7c7110d6c332bc
xuejiaming <326308290@qq.com>
2022-07-24 13:18:24 +0800

    1

---
+++ ShardingCoreMultiTenantSys/Migrations/Identity/20220724042931_IdentityInit.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShardingCoreMultiTenantSys.Migrations.Identity
+{
+    public partial class IdentityInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SysUser",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SysUser", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SysUserTenantConfig",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    UserId = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    ConfigJson = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SysUserTenantConfig", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SysUser");
+
+            migrationBuilder.DropTable(
+                name: "SysUserTenantConfig");
+        }
+    }
+}
--- ShardingCoreMultiTenantSys/MigrationsAssemblies/MultiDatabaseMigrationsAssembly.cs
+++ ShardingCoreMultiTenantSys/MigrationsAssemblies/MultiDatabaseMigrationsAssembly.cs
@@ -88,33 +88,20 @@ namespace ShardingCoreMultiTenantSys.MigrationsAssemblies;
         ///     doing so can result in application failures when updating to a new Entity Framework Core release.
         /// </summary>
         public virtual ModelSnapshot? ModelSnapshot
-            => GetMod();
+            => _modelSnapshot ??= (from t in Assembly.GetConstructibleTypes()
+                    where t.IsSubclassOf(typeof(ModelSnapshot))
+                          && MigrationNamespace.Equals(t?.Namespace)
+                          && t.GetCustomAttribute<DbContextAttribute>()?.ContextType == _contextType
+                    select (ModelSnapshot)Activator.CreateInstance(t.AsType())!)
+                .FirstOrDefault();
 
-        private ModelSnapshot GetMod()
-        {
-            Console.WriteLine("_modelSnapshot:"+ _modelSnapshot);
-            if (_modelSnapshot == null)
-            {
-                Console.WriteLine("_modelSnapshot:null");
-                _modelSnapshot = (from t in Assembly.GetConstructibleTypes()
-                        where t.IsSubclassOf(typeof(ModelSnapshot))
-                                                                    && MigrationNamespace.Equals(t?.Namespace)
-                                                                    && t.GetCustomAttribute<DbContextAttribute>()?.ContextType == _contextType
-                        select (ModelSnapshot)Activator.CreateInstance(t.AsType())!)
-                    .FirstOrDefault();
-
-                Console.WriteLine("_modelSnapshot:" + _modelSnapshot);
-            }
-            return _modelSnapshot;
-        }
-
-        /// <summary>
-        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
-        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
-        ///     any release. You should only use it directly in your code with extreme caution and knowing that
-        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
-        /// </summary>
-        public virtual Assembly Assembly { get; }
+    /// <summary>
+    ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+    ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+    ///     any release. You should only use it directly in your code with extreme caution and knowing that
+    ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+    /// </summary>
+    public virtual Assembly Assembly { get; }
 
         /// <summary>
         ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
